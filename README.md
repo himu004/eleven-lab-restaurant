1d5b588873645fcdc0e2a92043c97d47f5673e87
Himu004 <himu.chowdhury01@gmail.com>
2024-12-21 20:43:52 +0600

    A - 11 Project Added and Packages Installed

---
+++ README.md
@@ -0,0 +1,4 @@
+# Eleven Lab Restaurant 
+
+## Description
+This is a website for a restaurant called Eleven Lab.
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+import daisyUI from 'daisyui'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyUI
+  ],
+}
+
