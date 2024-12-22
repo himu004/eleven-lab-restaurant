5eb7b42db191709d7e06595b141e521b36137cad
Himu004 <himu.chowdhury01@gmail.com>
2024-12-23 00:16:45 +0600

    Register Page Completed with Auth Methods

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,242 @@
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/logo1.png";
+import { useContext, useState } from "react";
+import toast from "react-hot-toast";
+import { AuthContext } from "../../providers/Context";
+import Lottie from "react-lottie";
+import animationData from "../../assets/lottie/register_animation.json";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
-    return (
-        <div>
-            This is Register page
+  const navigate = useNavigate();
+  const { signInWithGoogle, createUser, updateUserProfile, setUser } =
+    useContext(AuthContext);
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const pass = form.password.value;
+
+    if (!validatePassword(pass)) {
+      setPasswordError(
+        "Password must have an uppercase letter, a lowercase letter, and be at least 6 characters long."
+      );
+      return;
+    }
+
+    try {
+      const result = await createUser(email, pass);
+      await updateUserProfile(name, photo);
+      setUser({ ...result.user, photoURL: photo, displayName: name });
+      toast.success("Signup Successful");
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      toast.error(err?.message);
+    }
+  };
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithGoogle();
+      toast.success("Signin Successful");
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      toast.error(err?.message);
+    }
+  };
+
+  const validatePassword = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    return regex.test(password);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    if (validatePassword(e.target.value)) {
+      setPasswordError("");
+    } else {
+      setPasswordError(
+        "Password must have an uppercase letter, a lowercase letter, and be at least 6 characters long."
+      );
+    }
+  };
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-[calc(100vh-306px)] my-12">
+      <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-base-100 rounded-lg shadow-lg lg:max-w-4xl">
+        <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
+          <div className="flex justify-center mx-auto">
+            <img className="w-auto h-28 sm:h-18" src={logo} alt="Logo" />
+          </div>
+
+          <p className="mt-3 text-xl text-center text-gray-600">
+            Wanna Eat, Get Onboard..
+          </p>
+
+          <div
+            onClick={handleGoogleSignIn}
+            className="flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50"
+          >
+            <div className="px-4 py-2">
+              <svg className="w-6 h-6" viewBox="0 0 40 40">
+                <path
+                  d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                  fill="#FFC107"
+                />
+                <path
+                  d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+                  fill="#FF3D00"
+                />
+                <path
+                  d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+                  fill="#4CAF50"
+                />
+                <path
+                  d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                  fill="#1976D2"
+                />
+              </svg>
+            </div>
+
+            <span className="w-5/6 px-4 py-3 font-bold text-center">
+              Sign in with Google
+            </span>
+          </div>
+
+          <div className="flex items-center justify-between mt-4">
+            <span className="w-1/5 border-b lg:w-1/4"></span>
+            <span className="text-xs text-center text-gray-500 uppercase hover:underline">
+              or Registration with email
+            </span>
+            <span className="w-1/5 border-b lg:w-1/4"></span>
+          </div>
+          <form onSubmit={handleSignUp}>
+            <div className="mt-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-600"
+               
+              >
+                Username
+              </label>
+              <input
+                id="name"
+                autoComplete="name"
+                name="name"
+                placeholder="Your Name"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                type="text"
+              />
+            </div>
+            <div className="mt-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-600"
+                htmlFor="LoggingEmailAddress"
+              >
+                Email Address
+              </label>
+              <input
+                id="LoggingEmailAddress"
+                autoComplete="email"
+                name="email"
+                placeholder="Your Email"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                type="email"
+              />
+            </div>
+            <div className="mt-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-600"
+                htmlFor="photo"
+              >
+                Photo URL
+              </label>
+              <input
+                id="photo"
+                autoComplete="photo"
+                name="photo"
+                placeholder="Your Photo URL"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                type="text"
+              />
+            </div>
+
+            <div className="mt-4">
+              <div className="flex justify-between">
+                <label
+                  className="block mb-2 text-sm font-medium text-gray-600"
+                  htmlFor="loggingPassword"
+                >
+                  Password
+                </label>
+                <button
+                  type="button"
+                  className="text-sm text-gray-600 focus:outline-none"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
+
+              <input
+                id="loggingPassword"
+                autoComplete="current-password"
+                name="password"
+                placeholder="Your Password"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              {passwordError && (
+                <p className="mt-2 text-sm text-red-600">{passwordError}</p>
+              )}
+            </div>
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          <div className="flex items-center justify-between mt-4">
+            <span className="w-1/5 border-b md:w-1/4"></span>
+
+            <Link
+              to="/login"
+              className="text-xs text-gray-500 uppercase hover:underline"
+            >
+              or sign in
+            </Link>
+
+            <span className="w-1/5 border-b md:w-1/4"></span>
+          </div>
         </div>
-    );
+        <div className="hidden lg:block lg:w-1/2">
+          <Lottie options={defaultOptions} height={400} width={400} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
